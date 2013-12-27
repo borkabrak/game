17e727f297162cb5d97cedadfd5a9a9000066bad
Jonathan Carter <jcarter@str.com>
2013-12-27 12:29:51 -0600

    Removed Square.attributes in favor of putting a square's attributes directly on the object

--- square.js
+++ square.js
@@ -1,51 +1,52 @@
-"use strict";
+// Squares -- players in the game
 
-var Square = function(init){
+var Square = function(init, entities){
+    "use strict";
     var my = this;
    
-    my.attributes = {
-        x: init.x,
-        y: init.y,
-        height: init.size,
-        width: init.size,
-        color: init.color,
-    }
+    my.x = init.x;
+    my.y = init.y;
+    my.height = init.size;
+    my.width = init.size;
+    my.color = init.color;
 
     var context = document.getElementById("display").getContext('2d');
 
     my.draw = function(){
-        context.fillStyle = my.attributes.color;
-        return context.fillRect( my.attributes.x, my.attributes.y, my.attributes.width, my.attributes.height );
+        context.fillStyle = my.color;
+        return context.fillRect( my.x, my.y, my.width, my.height );
     };
 
     my.clear = function(){
-        return context.clearRect( my.attributes.x, my.attributes.y, my.attributes.width, my.attributes.height );
+        return context.clearRect( my.x, my.y, my.width, my.height );
     }
 
     my.move = function(instruction){
 
-        var attr = my.attributes;
         instruction = instruction.replace(/ /g,"").split(",");
         my.clear();
 
-        attr[instruction[0]] += parseInt(instruction[1], 10);
+        my[instruction[0]] += parseInt(instruction[1], 10);
 
         // Prevent movement outside the canvas
-        if (attr.x < 0) {
-            attr.x = 0;
+        if (my.x < 0) {
+            my.x = 0;
         };
 
-        if (attr.y < 0) {
-            attr.y = 0;
+        if (my.y < 0) {
+            my.y = 0;
         };
 
-        if (attr.x + attr.width > context.canvas.width) {
-            attr.x = context.canvas.width - attr.width;
+        if (my.x + my.width > context.canvas.width) {
+            my.x = context.canvas.width - my.width;
         };
 
-        if (attr.y + attr.height > context.canvas.height) {
-            attr.y = context.canvas.height - attr.height;
+        if (my.y + my.height > context.canvas.height) {
+            my.y = context.canvas.height - my.height;
         };
+
+        // Detect collisions
+        entities.detect_collisions();
 
         return my.draw();
 
@@ -53,4 +54,5 @@ var Square = function(init){
 
     // initialize
     my.draw();
+    entities.register(my);
 };
